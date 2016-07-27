36d3da39085174345739d96c72769e677b873794
Craig Lewis <clewis-github.com@krod.org>
2016-07-26 18:50:33 -0700

    Allow arbitrary config options

--- ddclient/attributes/default.rb
+++ ddclient/attributes/default.rb
@@ -6,3 +6,5 @@ default[:ddclient][:protocol] = "dyndns2"
 default[:ddclient][:use] = "web, web=checkip.dyndns.com, web-skip='IP Address'"
 default[:ddclient][:server] = "members.dyndns.org"
 default[:ddclient][:interval] = "21600" #seconds
+
+default[:ddclient][:configs] = []
