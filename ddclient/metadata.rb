122422a56893355de276b2f3de9c5ee0df5ab118
Craig Lewis <clewis-github.com@krod.org>
2016-07-26 18:50:52 -0700

    Berks requires the metadata name

--- ddclient/metadata.rb
+++ ddclient/metadata.rb
@@ -4,3 +4,4 @@ license          "Apache 2.0"
 description      "Installs ddclient"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.0"
+name             "ddclient"
